ce419403086a2ccf3c9872442915ac3b7bff528c
jforbes <jeannette.forbes@gmail.com>
2017-09-18 14:29:17 -0400

    Timeouts and collisions ahoy

--- src/js/player.js
+++ src/js/player.js
@@ -8,26 +8,28 @@ Player = function(id, name, type){
 	this.id = id;
 	this.name = name || 'anonymous';
 	this.type = type || 0;
+	this.size = 25;
 
-	this.loc = new Victor(0,0); //this should use melonjs' me.Vector2d
+	this.loc = new Victor(0,0); // What's the vector, Victor?
 	this.rot = 0;
 
+	this.isColliding = false;
 	this.color = 'white';
 };
 
 Player.prototype.move = function(loc, prevLoc){
-	this.loc = {x:loc.x, y:loc.y};
+	this.loc = new Victor(loc.x, loc.y);
 	this.rot = 0; //this should calculate rotation
 };
 
 Player.prototype.draw = function(ctx) {
 	ctx.save();
 
-	ctx.fillStyle = this.color;
+	ctx.fillStyle = this.color || 'white';
 	
 	ctx.beginPath();
 
-	ctx.arc(this.loc.x, this.loc.y, 25, 0, 2*Math.PI);
+	ctx.arc(this.loc.x, this.loc.y, this.size, 0, 2*Math.PI);
 	ctx.translate(this.loc.x-20, this.loc.y+10);
 	ctx.rotate(this.rot);
 
@@ -35,3 +37,17 @@ Player.prototype.draw = function(ctx) {
 
 	ctx.restore();
 };
+
+Player.prototype.collide = function(bool){
+	this.isColliding = true;
+	this.color = 'red';
+}
+
+Player.prototype.checkCollision = function(collider){
+	var distBtwn = this.loc.distance(collider.loc);
+	if(distBtwn < (this.size/2 + collider.size/2)){
+		this.collide(true);
+		return true;
+	}
+	return false;
+}
